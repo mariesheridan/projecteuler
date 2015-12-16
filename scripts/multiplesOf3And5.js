2338f775b8761d6f4b0d491f41d374bd73faaa35
mariesheridan <marie.sheridan@yahoo.com>
2015-12-16 20:07:45 +0800

    Problem 1

---
+++ scripts/multiplesOf3And5.js
@@ -0,0 +1,15 @@
+function multiplesOf3And5(){
+    var input = document.getElementById("input1");
+    var max = input.elements[0].value;
+    var output = 0;
+
+    for (var i = 1; i < max; i++)
+    {
+        if (((i % 3) === 0) || ((i % 5) === 0))
+        {
+            output += i;
+        }
+    }
+    
+    document.getElementById("answer1").innerHTML = output;
+}
